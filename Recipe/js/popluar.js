f730197327f26f5ece6970e86a4d6aeacb71d051
Ohminjung0804 <w2009@e-mirim.hs.kr>
2022-02-06 15:26:02 +0900

    popular 슬라이드 작동

---
+++ Recipe/js/popluar.js
@@ -0,0 +1,12 @@
+$(function (){
+  $('.multiple-items').slick({
+    infinite : true,
+    slidesToShow : 3,
+    arrow : false,
+    autoplay : true,
+    autoplaySpeed : 1500,
+    dots : false,
+    centerMode : true,
+    centerPadding : '0'
+  });
+});
