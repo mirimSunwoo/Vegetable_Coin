7936d16e4b2dbd6cfccda70b34b74251fa739c18
Ohminjung0804 <w2009@e-mirim.hs.kr>
2022-02-08 17:18:17 +0900

    레시피 썸네일 연결

---
+++ Recipe/js/recipe_main.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+
+    $("#header").load("../header/header.html");   
+    $("#popular").load("popular.html"); 
+    $("#recent").load("recent.html"); 
+    $("#category").load("category.html");
+    $("#footer").load("../footer/footer.html");
+    });
+
+
+
