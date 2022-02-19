590bbe2a11debccb2ca46d98b0701ec136f5e0ba
Ohminjung0804 <w2009@e-mirim.hs.kr>
2022-02-19 23:43:03 +0900

    Solution js파일

---
+++ Solution/js/popular.js
@@ -0,0 +1,7 @@
+$(function (){
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      });
+  });
---
+++ Solution/js/solution_main.js
@@ -0,0 +1,6 @@
+$(document).ready( function() {
+
+    $("#header").load("../header/header.html");   
+    $(".grow_box").load("content_dtail.html");  
+    $("#footer").load("../footer/footer.html");
+    });
