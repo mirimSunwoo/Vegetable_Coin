bae16c5c3f8cc65281fd7ad83ada0445e632e497
w2009 <w2009@e-mirim.hs.kr>
2022-06-15 03:39:47 +0900

    recent vegetable : element remove

--- Solution/js/recent.js
+++ Solution/js/recent.js
@@ -6,20 +6,29 @@ function addItem(_src){
     recent_img.src = _src; //추가할 이미지 주소 넣음
     let delete_btn = document.createElement('input');
     delete_btn.src = 'image/recent_img/button.png';
+    delete_btn.onclick = removeItem;
 
-
+    //add css
     li.classList.add('recent_li');
+    li.id = 'recent_li';
     recent_items.classList.add('recent-items');
     recent_img.classList.add('recent-img');
+    recent_img.id = 'recent-img';
     delete_btn.classList.add('delete');
 
+    //add appendChild
     document.getElementById('recent-box').appendChild(li);
     li.appendChild(recent_items);
     recent_items.appendChild(recent_img);
     recent_items.appendChild(delete_btn);
-    // save(item_img);
+  
     console.log(1);
     console.log(li.className);
     console.log(recent_img.src);
     console.log(delete_btn.src);
+}
+
+function removeItem(){
+    let li = document.getElementById('recent_li');
+    li.remove();
 }
