6e5cc487a6a420d828d1bc0f4b84e0853620eb90
w2009 <w2009@e-mirim.hs.kr>
2022-06-19 17:12:16 +0900

    최근 본 레시피 완전히 지워지신다..ㅜㅠ

--- Solution/js/recent.js
+++ Solution/js/recent.js
@@ -15,9 +15,7 @@ function addItem(_src){
 
     let n = JSON.parse(localStorage.getItem('img_collection'));
     console.log(n.length);
-    if(n.length == 6){
-        n.splice(1);
-    }
+    
     for(let i = 1; i< n.length; i++){
         if(_src == n[i]){
             return;
@@ -35,53 +33,99 @@ function show_recent(){
         for(let i = 1; i< n.length; i++){
             
 
+            let create_id = 'recent_li'+i;
+            let img_id = 'recent-img'+i;
+            let btn_id = 'delete_btn'+i;
+            let li = document.createElement("li"); //li element 추가
+            let recent_items = document.createElement('div'); //item div element 추가
+            let recent_img = document.createElement("img"); //item img element 추가
+            recent_img.src = n[i]; //추가할 이미지 주소 넣음
+            let delete_btn = document.createElement('input');
+            delete_btn.src = './image/recent_img/button.png';
+            console.log(delete_btn.src);
+            delete_btn.addEventListener('click',function(delete_btn){
+                removeItem(delete_btn);
+            })
 
-        let li = document.createElement("li"); //li element 추가
-        let recent_items = document.createElement('div'); //item div element 추가
-        let recent_img = document.createElement("img"); //item img element 추가
-        recent_img.src = n[i]; //추가할 이미지 주소 넣음
-        let delete_btn = document.createElement('input');
-        delete_btn.src = './image/recent_img/button.png';
-        console.log(delete_btn.src);
-        delete_btn.onclick = removeItem;
+            //add id, class
+            li.id = create_id;
+            recent_img.id = img_id;
+            delete_btn.id = btn_id;
+            li.classList.add('recent_li');
+            recent_items.classList.add('recent-items');
+            recent_img.classList.add('recent-img');
+            delete_btn.classList.add('delete');
+            
+            
 
-        //add css
-        li.classList.add('recent_li');
-        li.id = 'recent_li';
-        // recent_img.id = 'recent_img';
-        recent_items.classList.add('recent-items');
-        recent_img.classList.add('recent-img');
-        recent_img.id = 'recent-img';
-        delete_btn.classList.add('delete');
+            //add appendChild
+            document.getElementById('recent-box').appendChild(li);
+            li.appendChild(recent_items);
+            recent_items.appendChild(recent_img);
+            recent_items.appendChild(delete_btn);
+        }
 
-        //add appendChild
-        document.getElementById('recent-box').appendChild(li);
-        li.appendChild(recent_items);
-        recent_items.appendChild(recent_img);
-        recent_items.appendChild(delete_btn);
-
+        if(n.length >= 7){
+            let img_collection = JSON.parse(localStorage.getItem('img_collection'));
+            img_collection.splice(1,1);
+            console.log(img_collection);
+            localStorage.setItem('img_collection',JSON.stringify(img_collection));
+        }
         // console.log(1);
         // console.log(li.className);
         // console.log(recent_img.src);
         // console.log(delete_btn.src);
-        }
+        
     }
     
 }
-function removeItem(){
-    let li = document.getElementById('recent_li');
-    let recent_img = document.getElementById('recent-img');
-    let img_src = recent_img.src;
+function removeItem(btn){
+    
+    console.log(btn.target);
+    let delete_btn = btn.target;
+    // let recent_li = document.getElementById(li_id);
+    // let recent_img = document.getElementById(img_id);
+    // let img_src = recent_img.src;
+    // let delete_btn = document.getElementById(btn_id);
+
+
+    //형제 요소
+    let bro= delete_btn.previousElementSibling;
+    console.log(bro.src);
+
+    //부모요소 li 지우기
+    var parent = delete_btn.parentNode;
+    var max_parent = parent.parentNode;
+    console.log(max_parent);
+    max_parent.remove();
 
     let img_collection = JSON.parse(localStorage.getItem('img_collection'));
-    console.log(img_collection);
     for(let i = 0; i < img_collection.length; i++ ){
-        if(img_src == img_collection[i]){
+        if(bro.src == img_collection[i]){
             img_collection.splice(i,1);
+            console.log(img_collection);
             localStorage.setItem('img_collection',JSON.stringify(img_collection));
         }
     }
-    li.remove();
+    console.log(img_collection);
+    
+
+    // let li = document.getElementById('recent_li');
+    
+
+    
+    //
+
+    
+    // console.log(img_collection);
+    // for(let i = 0; i < img_collection.length; i++ ){
+    //     if(img_src == img_collection[i]){
+    //         img_collection.splice(i,1);
+    //         console.log(img_collection);
+    //         localStorage.setItem('img_collection',JSON.stringify(img_collection));
+    //     }
+    // }
+    // li.remove();
 
 }
 
