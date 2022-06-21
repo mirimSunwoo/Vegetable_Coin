c1b2eb7ee6ada426f3fcb01f2253db0a10393bea
YunA486 <w2039@e-mirim.hs.kr>
2022-06-22 01:56:24 +0900

    Edit Buying

---
+++ V-coin/storage.js
@@ -0,0 +1,21 @@
+if (localStorage.getItem("potato") === null) {
+    localStorage.setItem("potato", 0);
+} else if(localStorage.getItem("sweetPotato") === null){
+    localStorage.setItem("sweetPotato", 0);
+} else if(localStorage.getItem("carrot") === null){
+    localStorage.setItem("carrot", 0);
+} else if(localStorage.getItem("radish") === null){
+    localStorage.setItem("radish", 0);
+} else if(localStorage.getItem("lettuce") === null){
+    localStorage.setItem("lettuce", 0);
+} else if(localStorage.getItem("spinach") === null){
+    localStorage.setItem("spinach", 0);
+} else if(localStorage.getItem("onion") === null){
+    localStorage.setItem("onion", 0);
+} else if(localStorage.getItem("cucumber") === null){
+    localStorage.setItem("cucumber", 0);
+} else if(localStorage.getItem("beanSprouts") === null){
+    localStorage.setItem("beanSprouts", 0);
+} else if(localStorage.getItem("tomato") === null){
+    localStorage.setItem("tomato", 0);
+}
